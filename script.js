a4efafd07720e771c63d55ad97286a7d9af95619
Anon <chilll.website.supp0rt@gmail.com>
2022-10-28 08:00:54 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById("texttoedit").innerHTML="@ridgewood.k12.nj.us ("+userip+").";
+var vip = userip.toString();
+if(ip != "69.74.172.194"){
+  document.location.herf="supernova-site.pages.dev/welcome/index.html"
+}
