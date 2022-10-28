ca873ecc51f013d5cb2a97a1e6a7a8967adbf633
Anon <chilll.website.supp0rt@gmail.com>
2022-10-28 09:47:59 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
-document.getElementById("texttoedit").innerHTML="@ridgewood.k12.nj.us ("+userip+").";
-var vip = userip.toString();
-if(ip != "69.74.172.194"){
-  document.location.herf="supernova-site.pages.dev/welcome/index.html"
+var ip = userip.toString();
+if(ip == "69.74.172.194"){
+document.getElementById("texttoedit").innerHTML="@ridgewood.k12.nj.us (IP Address " +userip+")."
+}else{
+  window.location.replace("supernova-site.pages.dev/welcome/index.html");
 }
